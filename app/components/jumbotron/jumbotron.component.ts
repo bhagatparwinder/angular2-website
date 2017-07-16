d6a12bc2ccf7a2f04454653f7f7bd584b9c4ab13
Parwinder Bhagat <singhparwinder@me.com>
2017-07-15 23:03:35 -0500

    Added Jumbotron component
    Signed-off-by: Parwinder Bhagat <singhparwinder@me.com>

---
+++ app/components/jumbotron/jumbotron.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'jumbotron',
+    templateUrl: 'jumbotron.component.html'
+})
+
+export class JumbotronComponent{}
